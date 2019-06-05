fdeefae6ffa3d4be46b7ab4350e1e8095afd7e24
Snehit Sah <48585950+snehitsah@users.noreply.github.com>
2019-06-05 14:36:29 +0530

    Add main.cpp
    Added main.cpp with an incomplete number generator function.

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/**
+    A Football game made using random numbers, much like Ludo.
+    Copyright (C) 2019  Snehit Sah
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getRandomNumber()
+{
+    int theRandomNumber;
+    /**Code to generate the random number
+     * The random number is saved 
+     * in variable theRandomNumber.
+     * Function returns this number
+     **/
+
+    return theRandomNumber;
+}
+
+int main()
+{
+    return 0;
+}
