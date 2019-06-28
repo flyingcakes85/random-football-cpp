513df807f3fdb209f2ca237e6e3d41fb06faf083
Snehit Sah <cdbusinesssnehi@gmail.com>
2019-06-28 11:59:22 +0530

    Update functionalities.h

--- functionalities.h
+++ functionalities.h
@@ -692,6 +692,7 @@ int startGame(int guesses, userData &player)
                 {
                     cout << "PC scored a goal!";
                     ++score[1];
+                    playerDistanceFromGoal = 3;
                     goto endOfLoop;
                 }
             }
